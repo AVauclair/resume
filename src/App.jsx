5af2f24d225d348570cf83715e9565a40939c89c
AVauclair <lyzha100kilo@mail.ru>
2024-09-12 16:57:52 +0700

    набросал раздел "хоум"

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import avatar from "/src/assets/avatar.jpg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello world</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>  
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <nav className="menu">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Образование</a>
+          </li>
+          <li>
+            <a>Проекты</a>
+          </li>
+        </nav>
+      </header>
+      <main>
+        <div className="home">
+          <div className="person">
+            <img src={avatar} className="avatar"></img>
+            <div>Давид Сироджов</div>
+            <div>React-разработчик</div>
+          </div>
+          <div className="about">
+            <h3>Обо мне</h3>
+            <div className="info">
+              <p>Возраст: 22</p>
+              <p>Навыки: HTML, CSS, TypeScript, React, Redux</p>
+              <p>Образование: техник-программист</p>{" "}
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
